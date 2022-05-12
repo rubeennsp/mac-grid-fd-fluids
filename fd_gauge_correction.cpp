d77bd9f921512fe8b528610d23de3173341f1762
Ruben Partono <rsparton@uwaterloo.ca>
2022-05-12 09:29:59 -0400

    Initial commit

---
+++ fd_gauge_correction.cpp
@@ -0,0 +1,463 @@
+#include <fftw3.h>
+#include <assert.h>
+#include <cmath>
+#include <vector>
+#include "fd_gauge_correction.h"
+
+
+//#############################################################################
+// Helpers to deal with odd vs. even signal differences
+//#############################################################################
+
+static const Symmetry streamfn_3d_symmetries_x[3] = { Symmetry::Even, Symmetry::Odd, Symmetry::Odd, };
+static const Symmetry streamfn_3d_symmetries_y[3] = { Symmetry::Odd, Symmetry::Even, Symmetry::Odd, };
+static const Symmetry streamfn_3d_symmetries_z[3] = { Symmetry::Odd, Symmetry::Odd, Symmetry::Even, };
+
+static const Symmetry velocity_3d_symmetries_x[3] = { Symmetry::Odd, Symmetry::Even, Symmetry::Even, };
+static const Symmetry velocity_3d_symmetries_y[3] = { Symmetry::Even, Symmetry::Odd, Symmetry::Even, };
+static const Symmetry velocity_3d_symmetries_z[3] = { Symmetry::Even, Symmetry::Even, Symmetry::Odd, };
+
+static const Symmetry phinodes_3d_symmetries[3] = { Symmetry::Odd, Symmetry::Odd, Symmetry::Odd };
+
+
+inline int array_size_for_fftw(int num_cells, Symmetry symmetry) {
+    // The number of cells along an axis is used as the half-period for the DCT/DST.
+    // The illustration below has 4 cells along the x-axis and 2 cells along the y-axis,
+    // regardless of whether we're dealing with quantities on the nodes, edges, or faces, and in 2d or 3d.
+    //    _ _ _ _
+    //   |_|_|_|_| 
+    //   |_|_|_|_|
+    //
+    switch (symmetry) {
+    case Symmetry::Even: return num_cells;
+    case Symmetry::Odd : return num_cells - 1; // For DST-I, the array length is one lower than the half-period.
+    }                                          // This is indeed the correct degree of freedom when considering the 0 boundaries.
+    throw 1;
+}
+
+/*
+    FFTW Documentation for DST/DCT types/kinds
+    https://www.fftw.org/fftw3_doc/1d-Real_002dodd-DFTs-_0028DSTs_0029.html
+    https://www.fftw.org/fftw3_doc/1d-Real_002deven-DFTs-_0028DCTs_0029.html
+*/
+
+inline fftw_r2r_kind r2r_kind_from_symmetry(Symmetry symmetry) {
+    switch (symmetry) {
+    case Symmetry::Even: return FFTW_REDFT10;
+    case Symmetry::Odd : return FFTW_RODFT00;
+    }
+    throw 1;
+}
+
+inline fftw_r2r_kind inverse_r2r_kind_from_symmetry(Symmetry symmetry) {
+    switch (symmetry) {
+    case Symmetry::Even: return FFTW_REDFT01;
+    case Symmetry::Odd : return FFTW_RODFT00;
+    }
+    throw 1;
+}
+
+
+
+//#############################################################################
+// General 3d grid DCT/DST and inverse.
+//#############################################################################
+
+
+// Works on interior psi values
+void to_frequencies_3d(
+    const int fluid_grid_size[3], // Cell count in each axis (NOT node count)
+    const Symmetry symmetries[3],
+    double *data, double *out
+) {
+    // Perform in-place if not provided with output location
+    if (!out) out = data;
+
+    // Build plan with the right sizes
+
+    const int freq_grid_size[3] = {
+        array_size_for_fftw(fluid_grid_size[0], symmetries[0]),
+        array_size_for_fftw(fluid_grid_size[1], symmetries[1]),
+        array_size_for_fftw(fluid_grid_size[2], symmetries[2]),
+    };
+
+    const fftw_r2r_kind transform_kinds[3] = {
+        r2r_kind_from_symmetry(symmetries[0]),
+        r2r_kind_from_symmetry(symmetries[1]),
+        r2r_kind_from_symmetry(symmetries[2]),
+    };
+
+    fftw_plan plan = fftw_plan_r2r_3d(
+        freq_grid_size[0], freq_grid_size[1], freq_grid_size[2],
+        data, out,
+        transform_kinds[0], transform_kinds[1], transform_kinds[2],
+        FFTW_ESTIMATE
+    );
+
+    // Execute
+    fftw_execute(plan);
+
+    // Cleanup
+    fftw_destroy_plan(plan);
+}
+
+
+void from_frequencies_3d(
+    const int fluid_grid_size[3], // Cell count in each axis (NOT node count)
+    const Symmetry symmetries[3],
+    double *data, double *out
+) {
+    // Perform in-place if not provided with output location
+    if (!out) out = data;
+
+    // Build plan with the right sizes
+
+    const int freq_grid_size[3] = {
+        array_size_for_fftw(fluid_grid_size[0], symmetries[0]),
+        array_size_for_fftw(fluid_grid_size[1], symmetries[1]),
+        array_size_for_fftw(fluid_grid_size[2], symmetries[2]),
+    };
+
+    const fftw_r2r_kind transform_kinds[3] = {
+        inverse_r2r_kind_from_symmetry(symmetries[0]),
+        inverse_r2r_kind_from_symmetry(symmetries[1]),
+        inverse_r2r_kind_from_symmetry(symmetries[2]),
+    };
+
+    fftw_plan plan = fftw_plan_r2r_3d(
+        freq_grid_size[0], freq_grid_size[1], freq_grid_size[2],
+        data, out,
+        transform_kinds[0], transform_kinds[1], transform_kinds[2],
+        FFTW_ESTIMATE
+    );
+
+    // Execute
+    fftw_execute(plan);
+
+    // Cleanup
+    fftw_destroy_plan(plan);
+
+    // IDST/IDCT needs to be downscaled by the full-period (ni, nj, nk are half-periods)
+    double multiplier = 1. / double(8 * fluid_grid_size[0] * fluid_grid_size[1] * fluid_grid_size[2]);
+    int num_freq_grid_entries = freq_grid_size[0] * freq_grid_size[1] * freq_grid_size[2];
+    for (int i = 0; i < num_freq_grid_entries; i++) {
+        out[i] *= multiplier;
+    }
+}
+
+
+
+//#############################################################################
+// Solvers in frequency domain
+//#############################################################################
+
+// Private vector helpers
+namespace {
+    struct vec3 { double x, y, z; };
+
+    inline double dot(vec3 v, vec3 w) {
+        return v.x * w.x + v.y * w.y + v.z * w.z;
+    }
+
+    inline double mag2(vec3 v) {
+        return dot(v, v);
+    }
+
+    inline vec3 cross(vec3 v, vec3 w) {
+        return {
+            v.y * w.z - v.z * w.y,
+            v.z * w.x - v.x * w.z,
+            v.x * w.y - v.y * w.x,
+        };
+    }
+
+    inline vec3 operator* (double c, vec3 v) {
+        return {
+            c * v.x,
+            c * v.y,
+            c * v.z,
+        };
+    }
+
+    inline vec3 operator* (vec3 v, double c) {
+        return c * v;
+    }
+
+    inline vec3 operator+ (vec3 v, vec3 w) {
+        return {
+            v.x + w.x,
+            v.y + w.y,
+            v.z + w.z,
+        };
+    }
+
+    inline vec3 operator- (vec3 v, vec3 w) {
+        return {
+            v.x - w.x,
+            v.y - w.y,
+            v.z - w.z,
+        };
+    }
+
+    inline vec3 operator- (vec3 v) {
+        return { -v.x, -v.y, -v.z, };
+    }
+}
+
+
+// Helper for indexing flat grids
+static const int INVALID_IDX = -1;
+inline int grid_index_3d(int ix, int iy, int iz, int lenx, int leny, int lenz) {
+    if (
+        ix >= 0 || iy >= 0 || iz >= 0
+        || ix < lenx || iy < leny || iz < lenz
+    )
+        return ix * leny * lenz + iy * lenz + iz;
+
+    return INVALID_IDX;
+}
+
+
+// TODO: Should we use sidelength multipliers at all?
+// - Might the cells have different sidelengths in the three axes?
+// - Might the actual correct values matter?
+
+// TODO: Explain what these diff multipliers are
+std::vector<double> fd_finite_differencing_multipliers_s2c(int n, double length_between_samples = 1) {
+    std::vector<double> v;
+    v.reserve(n);
+
+    for (int i = 0; i < n; i++) 
+        v.push_back(
+            2 * sin(double(i) * M_PI_2 / double(n))
+            / length_between_samples
+        );
+
+    return v;
+}
+
+
+void fd_node_poisson_solve_3d(
+    int ni, int nj, int nk,       // Cell count in each axis (NOT node count)
+    double *lap_freqs,            // Laplacian frequencies (ni-1, nj-1, nk-1)
+    double *node_freqs_out,       // (ni-1, nj-1, nk-1)
+    double cell_sidelength_x = 1,
+    double cell_sidelength_y = 1,
+    double cell_sidelength_z = 1
+) {
+    // Indexing
+    auto get_idx = [=](int i, int j, int k) { return grid_index_3d(i-1, j-1, k-1, ni-1, nj-1, nk-1); };
+
+    // Modify input array in-place if no output location is provided.
+    if (!node_freqs_out) node_freqs_out = lap_freqs;
+
+    // WARNING: If you modify the following, it has to take into account
+    // that the output location `node_freqs_out` might be the same as
+    // the input location `lap_freqs`.
+    // That is, do not read after writing.
+
+    // Finite differencing multipliers
+    std::vector<double> diff_s2c_x = fd_finite_differencing_multipliers_s2c(ni, cell_sidelength_x);
+    std::vector<double> diff_s2c_y = fd_finite_differencing_multipliers_s2c(nj, cell_sidelength_y);
+    std::vector<double> diff_s2c_z = fd_finite_differencing_multipliers_s2c(nk, cell_sidelength_z);
+
+    // The for loops here can start at 1 because all axes use DST-I and don't have the 0 frequency.
+    for (int i = 1; i < ni; i++) {
+        for (int j = 1; j < nj; j++) {
+            for (int k = 1; k < nk; k++) {
+                int node_idx = get_idx(i, j, k);
+                assert(node_idx != INVALID_IDX); // True because the for loop ranges are correct. Assert anyways.
+
+                // TODO: Explain what these diff vector is, lol
+                vec3 diff_s2c_ijk {
+                    diff_s2c_x[i],
+                    diff_s2c_y[j],
+                    diff_s2c_z[k],
+                };
+                vec3 diff_c2s_ijk = -diff_s2c_ijk;
+
+                double div_multiplier = dot(diff_c2s_ijk, diff_s2c_ijk);
+
+                double node_freq_answer = lap_freqs[node_idx] / div_multiplier;
+
+                node_freqs_out[node_idx] = node_freq_answer;
+            }
+        }
+    }
+}
+
+
+void fd_gauge_correct_3d(
+    int ni, int nj, int nk,            // Cell count in each axis (NOT node count)
+    double *psi_freqs_x,               // (ni, nj-1, nk-1)
+    double *psi_freqs_y,               // (ni-1, nj, nk-1)
+    double *psi_freqs_z,               // (ni-1, nj-1, nk)
+    double *psi_freqs_x_out = nullptr, // (ni, nj-1, nk-1)
+    double *psi_freqs_y_out = nullptr, // (ni-1, nj, nk-1)
+    double *psi_freqs_z_out = nullptr, // (ni-1, nj-1, nk)
+    double *phi_freqs_out = nullptr,   // (ni-1, nj-1, nk-1)
+    double cell_sidelength_x = 1,
+    double cell_sidelength_y = 1,          
+    double cell_sidelength_z = 1
+) {
+    // Indexing
+    auto psi_x_idx = [=](int i, int j, int k) { return grid_index_3d(i  , j-1, k-1, ni  , nj-1, nk-1); };
+    auto psi_y_idx = [=](int i, int j, int k) { return grid_index_3d(i-1, j  , k-1, ni-1, nj  , nk-1); };
+    auto psi_z_idx = [=](int i, int j, int k) { return grid_index_3d(i-1, j-1, k  , ni-1, nj-1, nk  ); };
+
+    auto phi_idx   = [=](int i, int j, int k) { return grid_index_3d(i-1, j-1, k-1, ni-1, nj-1, nk-1); };
+
+    // Modify input array in-place if no output location is provided.
+    if (!psi_freqs_x_out) psi_freqs_x_out = psi_freqs_x;
+    if (!psi_freqs_y_out) psi_freqs_y_out = psi_freqs_y;
+    if (!psi_freqs_z_out) psi_freqs_z_out = psi_freqs_z;
+
+    // Finite differencing multipliers
+    std::vector<double> diff_s2c_x = fd_finite_differencing_multipliers_s2c(ni, cell_sidelength_x);
+    std::vector<double> diff_s2c_y = fd_finite_differencing_multipliers_s2c(nj, cell_sidelength_y);
+    std::vector<double> diff_s2c_z = fd_finite_differencing_multipliers_s2c(nk, cell_sidelength_z);
+
+    // WARNING: If you modify the following, it has to take into account
+    // that the output location `psi_freqs_x_out` might be the same as
+    // the input location `psi_freqs_x`.
+    // That is, do not read after writing.
+
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++) {
+            for (int k = 0; k < nk; k++) {
+                int psi_x_idx_ijk = psi_x_idx(i, j, k);
+                int psi_y_idx_ijk = psi_y_idx(i, j, k);
+                int psi_z_idx_ijk = psi_z_idx(i, j, k);
+
+                vec3 psi_ijk {
+                    psi_x_idx_ijk == INVALID_IDX ? 0 : psi_freqs_x[psi_x_idx_ijk],
+                    psi_y_idx_ijk == INVALID_IDX ? 0 : psi_freqs_y[psi_y_idx_ijk],
+                    psi_z_idx_ijk == INVALID_IDX ? 0 : psi_freqs_z[psi_z_idx_ijk],
+                };
+
+                // TODO: Explain what these diff vector is, lol
+                vec3 diff_s2c_ijk {
+                    diff_s2c_x[i],
+                    diff_s2c_y[j],
+                    diff_s2c_z[k],
+                };
+                vec3 diff_c2s_ijk = - diff_s2c_ijk;
+
+                double phi_ijk = - dot(diff_c2s_ijk, psi_ijk) / dot(diff_c2s_ijk, diff_s2c_ijk);
+
+                vec3 grad_phi_ijk = phi_ijk * diff_s2c_ijk;
+
+                vec3 corrected_psi_ijk = psi_ijk - grad_phi_ijk;
+
+                // Write out to psi
+                if (psi_x_idx_ijk != INVALID_IDX) psi_freqs_x_out[psi_x_idx_ijk] = corrected_psi_ijk.x;
+                if (psi_y_idx_ijk != INVALID_IDX) psi_freqs_y_out[psi_y_idx_ijk] = corrected_psi_ijk.y;
+                if (psi_z_idx_ijk != INVALID_IDX) psi_freqs_z_out[psi_z_idx_ijk] = corrected_psi_ijk.z;
+                
+                if (phi_freqs_out) {
+                    int phi_idx_ijk = phi_idx(i, j, k);
+                    if (phi_idx_ijk != INVALID_IDX) phi_freqs_out[phi_idx_ijk] = phi_ijk;
+                }
+            }
+        }
+    }
+}
+
+
+
+//#############################################################################
+// 3d grid frequency domain transforms and inverses specific to certain fluid data.
+// 
+// Maybe we don't need every single one of these but they're a good reference
+// to help understand what's going on with the odd/even symmetries anyways.
+//#############################################################################
+
+
+// Works on interior psi values
+void to_frequencies_edges_3d(
+    int ni, int nj, int nk,
+    double *psi_x,                // (ni, nj-1, nk-1)
+    double *psi_y,                // (ni-1, nj, nk-1)
+    double *psi_z,                // (ni-1, nj-1, nk)
+    double *psi_x_out = nullptr,  // (ni, nj-1, nk-1)
+    double *psi_y_out = nullptr,  // (ni-1, nj, nk-1)
+    double *psi_z_out = nullptr   // (ni-1, nj-1, nk)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    to_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_x, psi_x, psi_x_out);
+    to_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_y, psi_y, psi_y_out);
+    to_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_z, psi_z, psi_z_out);
+}
+
+
+// Works on interior psi values
+void from_frequencies_streamfn_3d(
+    int ni, int nj, int nk,
+    double *psi_x,                // (ni, nj-1, nk-1)
+    double *psi_y,                // (ni-1, nj, nk-1)
+    double *psi_z,                // (ni-1, nj-1, nk)
+    double *psi_x_out = nullptr,  // (ni, nj-1, nk-1)
+    double *psi_y_out = nullptr,  // (ni-1, nj, nk-1)
+    double *psi_z_out = nullptr   // (ni-1, nj-1, nk)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    from_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_x, psi_x, psi_x_out);
+    from_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_y, psi_y, psi_y_out);
+    from_frequencies_3d(fluid_grid_size, streamfn_3d_symmetries_z, psi_z, psi_z_out);
+}
+
+
+// Works on interior u values
+void to_frequencies_velocity_3d(
+    int ni, int nj, int nk,
+    double *u_x,                // (ni-1, nj, nk)
+    double *u_y,                // (ni, nj-1, nk)
+    double *u_z,                // (ni, nj, nk-1)
+    double *u_x_out = nullptr,  // (ni-1, nj, nk)
+    double *u_y_out = nullptr,  // (ni, nj-1, nk)
+    double *u_z_out = nullptr   // (ni, nj, nk-1)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    to_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_x, u_x, u_x_out);
+    to_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_y, u_y, u_y_out);
+    to_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_z, u_z, u_z_out);
+}
+
+
+// Works on interior u values
+void from_frequencies_velocity_3d(
+    int ni, int nj, int nk,
+    double *u_x,                // (ni-1, nj, nk)
+    double *u_y,                // (ni, nj-1, nk)
+    double *u_z,                // (ni, nj, nk-1)
+    double *u_x_out = nullptr,  // (ni-1, nj, nk)
+    double *u_y_out = nullptr,  // (ni, nj-1, nk)
+    double *u_z_out = nullptr   // (ni, nj, nk-1)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    from_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_x, u_x, u_x_out);
+    from_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_y, u_y, u_y_out);
+    from_frequencies_3d(fluid_grid_size, velocity_3d_symmetries_z, u_z, u_z_out);
+}
+
+
+// Works on interior phi values
+void to_frequencies_phi_3d(
+    int ni, int nj, int nk,     // Cell count in each axis (NOT node count)
+    double *phi,                // (ni-1, nj-1, nk-1)
+    double *phi_out = nullptr   // (ni-1, nj-1, nk-1)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    to_frequencies_3d(fluid_grid_size, phinodes_3d_symmetries, phi, phi_out);
+}
+
+
+// Works on interior phi values
+void from_frequencies_phi_3d(
+    int ni, int nj, int nk,     // Cell count in each axis (NOT node count)
+    double *phi,                // (ni-1, nj-1, nk-1)
+    double *phi_out = nullptr   // (ni-1, nj-1, nk-1)
+) {
+    int fluid_grid_size[3] = {ni, nj, nk};
+    from_frequencies_3d(fluid_grid_size, phinodes_3d_symmetries, phi, phi_out);
+}
---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include "fd_gauge_correction.h"
+#include <iostream>
+
+void print_grid_3d(int ni, int nj, int nk, double *grid) {
+    for (int i = 0; i < ni; i++) {
+        for (int j = 0; j < nj; j++) {
+            for (int k = 0; k < nk; k++) {
+                std::cout << grid[i*nj*nk + j*nk + k] << " ";
+            }
+            std::cout << std::endl;
+        }
+        std:: cout << std::endl;
+    }
+}
+
+void test1() {
+
+    double grid[3][5][7] = {
+        {
+            {111, 112, 113, 114, 115, 116, 117, }, 
+            {121, 122, 123, 124, 125, 126, 127, }, 
+            {131, 132, 133, 134, 135, 136, 137, }, 
+            {141, 142, 143, 144, 145, 146, 147, }, 
+            {151, 152, 153, 154, 155, 156, 157, },
+        },
+        {
+            {211, 212, 213, 214, 215, 216, 217, }, 
+            {221, 222, 223, 224, 225, 226, 227, }, 
+            {231, 232, 233, 234, 235, 236, 237, }, 
+            {241, 242, 243, 244, 245, 246, 247, }, 
+            {251, 252, 253, 254, 255, 256, 257, },
+        },
+        {
+            {311, 312, 313, 314, 315, 316, 317, }, 
+            {321, 322, 323, 324, 325, 326, 327, }, 
+            {331, 332, 333, 334, 335, 336, 337, }, 
+            {341, 342, 343, 344, 345, 346, 347, }, 
+            {351, 352, 353, 354, 355, 356, 357, },
+        },
+    };
+
+    std::cout << "Original" << std::endl;
+    print_grid_3d(3, 5, 7, &grid[0][0][0]);
+    std::cout << std::endl;
+
+    // Convert into frequencies in place
+    to_frequencies_phi_3d(4, 6, 8, &grid[0][0][0]);
+
+    std::cout << "Frequencies" << std::endl;
+    print_grid_3d(3, 5, 7, &grid[0][0][0]);
+    std::cout << std::endl;
+
+    // Convert back into original data in place
+    from_frequencies_phi_3d(4, 6, 8, &grid[0][0][0]);
+
+    std::cout << "Reconstructed" << std::endl;
+    print_grid_3d(3, 5, 7, &grid[0][0][0]);
+    std::cout << std::endl;
+}
+
+int main() {
+    test1();
+}
